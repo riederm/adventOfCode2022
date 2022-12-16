184086f047041d5f22c4678c4e211a97405c1165
Mathias Rieder <mathias.rieder@gmail.com>
2022-12-16 10:33:22 +0000

    day16 a

--- day16/day16.ts
+++ day16/day16.ts
@@ -3,25 +3,45 @@ import * as fs from 'fs';
 
 
 let lines = fs
-    .readFileSync('input.test.txt').toString()
+    .readFileSync('input.txt').toString()
     .split("\n");
 
-class Edge{
-    constructor(public target: string, public cost: number){}
+class Edge {
+    constructor(public target: string, public cost: number) { }
 }
 
 let flowRates = new Map<string, number>();
 let edges = new Map<string, Edge[]>();
 
-function getSpanTreeFor(start: string, edges: Map<string, number[]>) : Map<string, number> {
-    let queue : string[] = [];
+function dijkstra(start: string) {
+    let distance = new Map<string, number>();
+    let q: string[] = [];
+    for (const k of flowRates.keys()) {
+        distance.set(k, Infinity);
+        q.push(k);
+    }
+    distance.set(start, 0);
+    q.sort((a, b) => distance.get(a) - distance.get(b));
+
+
+    while (q.length > 0) {
+        let current = q.shift();
+        for (const n of edges.get(current).filter(it => q.indexOf(it.target) >= 0)) {
+            let alt = distance.get(current) + n.cost;
+            if (alt < distance.get(n.target)) {
+                distance.set(n.target, alt);
+                q.sort((a, b) => distance.get(a) - distance.get(b));
+            }
+        }
+    }
+    return distance;
 }
 
 for (const l of lines) {
     let groups = /Valve (.+) has flow rate=(.+); tunnels? leads? to valves? (.*)/gm.exec(l);
-    let name = ''+groups[1]; 
+    let name = '' + groups[1];
     let rate = +groups[2];
-    let neighbours = (''+groups[3]).split(", ");
+    let neighbours = ('' + groups[3]).split(", ");
 
     edges.set(name, neighbours.map(it => new Edge(it, 1)));
     flowRates.set(name, rate);
@@ -29,41 +49,35 @@ for (const l of lines) {
 
 //expand edges
 for (const n of edges.keys()) {
-    let knownTargets = new Set(...edges.get(n).map(it => it.target));
-    for (const nn of edges.keys()) {
-        if (!knownTargets.has(nn)) {
+    let expandedEdges = dijkstra(n);
+    let targets = Array.from(expandedEdges.entries())
+        .map(([t, c]) => new Edge(t, c))
+        .filter(it => it.cost > 0 && it.cost < 9999999999);
 
-        }
-    }
-
-    
-    
+    edges.set(n, targets)
 }
 
-console.log(flowRates);;
+console.log(flowRates.size);;
 console.log(edges);
 
 let max = -1;
+function walk(pos: string, pressure: number, remainingMinutes: number, visited: Set<string>) {
+    if (remainingMinutes <= 0) {
+        return pressure;
+    }
 
-function walk(pos: string, pressure: number, remainingMinutes: number, visited: Set<string>){
-    // end?
-    if (remainingMinutes==0){
-        return
-    }
-    pressure += flowRates.get(pos)*Math.max(0,(remainingMinutes-1)); //we just opened it, it will flow from next minute on
-    if (pressure > max) {
-        max = pressure
-        console.log(max, remainingMinutes);
-        
-    }
-    
-    let neighbous = edges.get(pos);
+    pressure += flowRates.get(pos) * remainingMinutes; //we just opened it, it will flow from next minute on
+    max = Math.max(max, pressure);
+
+    let neighbous = edges.get(pos).filter(it => !visited.has(it.target))
+        .filter(it => flowRates.get(it.target) > 0);
+
     for (const n of neighbous) {
-        visited.add(n);
-        walk(n, pressure, remainingMinutes-1);
-        visited.delete(n);
-    }    
+        visited.add(n.target);
+        walk(n.target, pressure, remainingMinutes - (n.cost + 1), visited);
+        visited.delete(n.target);
+    }
 }
-
-walk("AA", 0, 30);
+max = -1;
+walk("AA", 0, 30, new Set());
 console.log(max);
